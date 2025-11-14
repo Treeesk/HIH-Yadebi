e2ab1ce2e3840447cb07124d41e52c651aa6d593
Ivan_Murogin <muroginivan@gmail.com>
2025-11-14 23:59:25 +0300

    add postgres.go

--- backend/internal/database/postgres.go
+++ backend/internal/database/postgres.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"ginserv/internal/config"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+func Connect(cfg *config.ConfigDB) *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("postregsql connection error: %v", err)
+	}
+	// defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("postgresql ping error: %v", err)
+	}
+	log.Println("postgresql succesfully connected")
+	return db
+}
