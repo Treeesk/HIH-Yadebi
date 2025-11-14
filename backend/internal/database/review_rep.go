47cc93323028ad86ebcbd261ec0a2c37b37b236c
Ivan_Murogin <muroginivan@gmail.com>
2025-11-15 01:58:49 +0300

    add review_rep

---
+++ backend/internal/database/review_rep.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"hih-yadebi-backend/internal/models"
+)
+
+type ReviewRepository struct {
+	DB *sql.DB
+}
+
+func NewReviewRepository(db *sql.DB) *ReviewRepository {
+	return &ReviewRepository{DB: db}
+}
+
+func (r *ReviewRepository) CreateReview(ctx context.Context, review *models.Review) error {
+	query := `
+		INSERT INTO reviews (user_id, app_id, score, comment, helpful)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id
+	`
+
+	return r.DB.QueryRowContext(ctx, query,
+		review.UserID,
+		review.AppID,
+		review.Score,
+		review.Comment,
+		review.Helpful,
+	).Scan(&review.ID)
+}
+
+func (r *ReviewRepository) GetReviewsByApp(ctx context.Context, appID int) ([]models.Review, error) {
+	rows, err := r.DB.QueryContext(ctx, `
+		SELECT id, user_id, app_id, score, comment, helpful
+		FROM reviews WHERE app_id = $1
+	`, appID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var list []models.Review
+	for rows.Next() {
+		var r models.Review
+		if err := rows.Scan(&r.ID, &r.UserID, &r.AppID, &r.Score, &r.Comment, &r.Helpful); err != nil {
+			return nil, err
+		}
+		list = append(list, r)
+	}
+
+	return list, nil
+}
