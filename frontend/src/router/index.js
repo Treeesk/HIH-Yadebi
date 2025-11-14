ec78284e6903f29acac3262aed54746b5346e09e
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-11-14 15:55:19 +0300

    Refactor code

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AppList from '../Pages/HomeView.vue' // Новые Pages страницы сюда подключать здесь
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+const routes = [
+    { path: '/', component: AppList },
+]
+
+export default createRouter({
+    history: createWebHistory(),
+    routes,
 })
-
-export default router
